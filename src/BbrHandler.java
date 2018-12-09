f21de4414ea3b896085a12da8b1e0179325015f3
Xofrats <firefox225@hotmail.com>
2018-12-09 11:40:36 +0100

    first1

--- src/BbrHandler.java
+++ src/BbrHandler.java
@@ -1,5 +1,6 @@
+//BBR Handler
 public class BbrHandler {
-    public static void createBbrManuelt(int propertyNumber, String adress, int houseNumber, int zipCode, double heatedSquareMeters, String propertyType){
+        public static void createBbrManuelt(int propertyNumber, String adress, int houseNumber, int zipCode, double heatedSquareMeters, String propertyType){
         Bbr createBbr = new Bbr(propertyNumber, adress, houseNumber,zipCode,heatedSquareMeters,propertyType);
     }
 }
