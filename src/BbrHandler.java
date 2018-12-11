726b42a86b49a8ae6f0aab7a1feb99498ebbd830
Xofrats <firefox225@hotmail.com>
2018-12-11 11:29:09 +0100

    storing bbr as they are created

--- src/BbrHandler.java
+++ src/BbrHandler.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+
 //BBR Handler
 public class BbrHandler {
+    private static ArrayList<Bbr> allBbr = new ArrayList<>();
+
         public static Bbr createBbrManuelt(int propertyNumber, String adress, int houseNumber, int zipCode, double heatedSquareMeters, String propertyType){
         Bbr createBbr = new Bbr(propertyNumber, adress, houseNumber,zipCode,heatedSquareMeters,propertyType);
+        allBbr.add(createBbr);
         return createBbr;
+
+    }
+
+    public static ArrayList<Bbr> getAllBbr(){
+            return allBbr;
     }
 }
