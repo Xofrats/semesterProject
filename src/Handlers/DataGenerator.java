d327ee4ac36449ded1955b100667d1f5343dc5ed
Xofrats <firefox225@hotmail.com>
2018-12-12 10:00:09 +0100

    Getting both projects up to date

--- src/Handlers/DataGenerator.java
+++ src/Handlers/DataGenerator.java
@@ -1,6 +1,13 @@
 package Handlers;
 
+import java.time.LocalDate;
+import java.util.Random;
+
 public class DataGenerator {
+
+    public static void randomDate(){
+        Random random = new Random();
+}
 
 
 
