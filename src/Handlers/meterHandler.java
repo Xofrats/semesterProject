5cab317a14f614a20e4ee3c7cba65f330bec098b
Xofrats <firefox225@hotmail.com>
2018-12-13 12:03:32 +0100

    MeterHandler

--- src/Handlers/meterHandler.java
+++ src/Handlers/meterHandler.java
@@ -3,15 +3,59 @@ package Handlers;
 import Model.BBR.Bbr;
 import Model.Meter.Meter;
 
+import java.util.ArrayList;
+
 public class meterHandler {
 
-    public static Meter createMeterAuto(String measureType, int number, int isActive, Bbr BBR){
+    private static ArrayList<Meter> allMeters = new ArrayList<>();
+
+    public static Meter createMeterAuto(String measureType, int number, int isActive, Bbr BBR) {
         Meter createdMeter = new Meter(number, isActive, BBR);
+        allMeters.add(createdMeter);
         return createdMeter;
     }
 
-    public static Meter createMeterManuelt(String measureType, int number, int isActive, int propertyNumber, String address, int houseNumber, int zipCode, double heatedSquareMeter, String propertyType){
-        Meter createdMeter = new Meter(number, isActive, BbrHandler.createBbrManuelt(propertyNumber, address, houseNumber, zipCode, heatedSquareMeter, propertyType) );
+    public static Meter createMeterManuelt(String measureType, int number, int isActive, int propertyNumber, String address, int houseNumber, int zipCode, double heatedSquareMeter, String propertyType) {
+        Meter createdMeter = new Meter(number, isActive, BbrHandler.createBbrManuelt(propertyNumber, address, houseNumber, zipCode, heatedSquareMeter, propertyType));
+        allMeters.add(createdMeter);
         return createdMeter;
     }
+
+    public static ArrayList<Meter> getAllMeters() {
+        return allMeters;
+    }
+
+    public static Meter getMeter(int meterNumber) {
+
+        for (Meter meter : allMeters) {
+            if (meterNumber == meter.getNumber()) {
+                return meter;
+            }
+        }
+        Meter falseMeter = new Meter(meterNumber, 0, new Bbr(0000, "nowhere", 00, 0000, 00, "none"));
+        return falseMeter;
+    }
+
+    public static void addBbrToMeter(int meterNumber, Bbr BBR) {
+
+        for (Meter meter : allMeters) {
+            if (meterNumber == meter.getNumber()) {
+                meter.setLocation(BBR);
+            }
+        }
+
+    }
+
+    public static void toggleActive(int meterNumber){
+        for (Meter meter : allMeters) {
+            if (meterNumber == meter.getNumber()) {
+               if(meter.getIsActive() == 1){
+                   meter.setIsActive(0);
+               } else {
+                   meter.setIsActive(1);
+               }
+            }
+        }
+    }
 }
+
